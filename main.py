1f200f6e38cf72491d2442159f699ed7eb71663c
Gavin Guinn <gavinguinn1@gmail.com>
2025-04-18 01:02:54 -0600

    workds

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# main.py
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello, Railway!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Railway injects PORT at runtime
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
